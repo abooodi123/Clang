5ef436d4e3c1f022b6e7ee3f6923ccc3e11d505a
Abdullah Ewidah <abdullahewidah@gmail.com>
2026-06-25 14:49:48 +0300

    added new file .c in branch 2

---
+++ 0_helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(){
+
+    puts("Hello from\t'puts'");// It's a standard C libarary function that writes a string argument to the stdout stream and append a newline char
+    printf("%s\n","hello from\t\"printf\"");
+    return 0;
+}
